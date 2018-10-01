57147901214b03631416dc5ed6914aec1c28af45
rjabdurrahman <md.rahman0296@gmail.com>
2018-10-02 00:27:51 +0600

    Fixed: Account list by group

--- pages/account.js
+++ pages/account.js
@@ -1,17 +1,26 @@
 app.controller('AccountCntlr', function ($scope) {
     $scope.accounts = {
-        CA : 'Current Asset',
-        NCA : 'Non-Current Asset',
-        CL : 'Current Liability',
-        NCL : 'Non-Current Liability',
-        EQTY : 'Equity',
-        EX : 'Experience',
-        REV : 'Revenue',
-        WD : 'Withdrawal',
-        SD : 'Contra Revenue',
-        COGS : 'Cost of Goods Sold',
-        WIP : 'Production',
-        CN : 'Contra'
+        CA: 'Current Asset',
+        NCA: 'Non-Current Asset',
+        CL: 'Current Liability',
+        NCL: 'Non-Current Liability',
+        EQTY: 'Equity',
+        EX: 'Experience',
+        REV: 'Revenue',
+        WD: 'Withdrawal',
+        SD: 'Contra Revenue',
+        COGS: 'Cost of Goods Sold',
+        WIP: 'Production',
+        CN: 'Contra'
     }
-    $scope.comAccounts = lsExGJInit('comAccounts', []);
+    $scope.comAccounts = [];
+    var accs = lsExGJInit('comAccounts', []);
+    var accLength = lsExGJInit('comAccounts', []).length;
+    for(acc in $scope.accounts){
+        for (i = 0; i < accLength; i++) {
+            if(accs[i].nature == $scope.accounts[acc]){
+                $scope.comAccounts.push(accs[i]);
+            };
+        }
+    }
 });
