c83ac5c23e50c80216ce7c3a3ffa4fb74dffa62a
rjabdurrahman <md.rahman0296@gmail.com>
2018-10-07 17:45:33 +0600

    Added: Some pages routing

--- app.js
+++ app.js
@@ -15,8 +15,57 @@ app.config(function($routeProvider) {
     templateUrl : 'pages/receivable.html',
     controller  : 'ReceivableCntlr'
   })
+  .when('/payable', {
+    templateUrl : 'pages/payable.html',
+    controller  : 'PayableCntlr'
+  })
+  .when('/companyheads', {
+    templateUrl : 'pages/company_heads.html',
+    controller  : 'CompanyHeadsCntlr'
+  })
+  .when('/costcenter', {
+    templateUrl : 'pages/cost_center.html',
+    controller  : 'CostCenterCntlr'
+  })
+  .when('/trucktractor', {
+    templateUrl : 'pages/truck_tractor.html',
+    controller  : 'TruckTractorCntlr'
+  })
+  .when('/employees', {
+    templateUrl : 'pages/employees.html',
+    controller  : 'EmployeesCntlr'
+  })
+  .when('/bank', {
+    templateUrl : 'pages/bank.html',
+    controller  : 'BankCntlr'
+  })
+  .when('/contractors', {
+    templateUrl : 'pages/contractors.html',
+    controller  : 'ContractorsCntlr'
+  })
   .otherwise({redirectTo: '/'});
 });
 app.controller('LoginCntlr', function($scope){
     $scope.message = "Login Cntl";
+});
+app.controller('PayableCntlr', function($scope){
+  $scope.message = "Payable Cntl";
+});
+app.controller('CompanyHeadsCntlr', function($scope){
+  $scope.message = "CompanyHeads Cntl";
+});
+app.controller('CostCenterCntlr', function($scope){
+  $scope.message = "CostCenter Cntl";
+});
+app.controller('TruckTractorCntlr', function($scope){
+  $scope.message = "TruckTractor Cntl";
+});
+app.controller('EmployeesCntlr', function($scope){
+  $scope.message = "Employees Cntl";
+});
+app.controller('BankCntlr', function($scope){
+  $scope.message = "Bank Cntl";
+});
+app.controller('ContractorsCntlr', function($scope){
+  $scope.message = "Contractors Cntl";
 });
