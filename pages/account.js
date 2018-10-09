b00571db6d42eaf9b7ff66776f21304b12ce500a
rjabdurrahman <md.rahman0296@gmail.com>
2018-10-09 16:20:05 +0600

    Added and Solved some data for min max of account code

--- pages/account.js
+++ pages/account.js
@@ -1,24 +1,25 @@
 app.controller('AccountCntlr', function ($scope) {
     $scope.accounts = {
-        CA: 'Current Asset',
-        NCA: 'Non-Current Asset',
-        CL: 'Current Liability',
-        NCL: 'Non-Current Liability',
-        EQTY: 'Equity',
-        EX: 'Experience',
-        REV: 'Revenue',
-        WD: 'Withdrawal',
-        SD: 'Contra Revenue',
-        COGS: 'Cost of Goods Sold',
-        WIP: 'Production',
-        CN: 'Contra'
+        CA: { name: 'Current Asset', min: 100, max: 199 },
+        NCA: { name: 'Non-Current Asset', min: 200, max: 299 },
+        CL: { name: 'Current Liability', min: 300, max: 399 },
+        NCL: { name: 'Non-Current Liability', min: 400, max: 499 },
+        EQTY: { name: 'Equity', min: 500, max: 599 },
+        EX: { name: 'Experience', min: 600, max: 699 },
+        REV: { name: 'Revenue', min: 700, max: 799 },
+        WD: { name: 'Withdrawal', min: 800, max: 900 },
+        SD: { name : 'Contra Revenue', min: 800, max: 899 },
+        COGS: { name : 'Cost of Goods Sold', min: 9, max:  900},
+        WIP: {name : 'Production', min: 900, max: 901},
+        CN: { name: 'Contra', min: 901, max: 999}
     }
+    lsSetJ('accCodes', $scope.accounts);
     $scope.comAccounts = [];
     var accs = lsExGJInit('comAccounts', []);
     var accLength = lsExGJInit('comAccounts', []).length;
-    for(acc in $scope.accounts){
+    for (acc in $scope.accounts) {
         for (i = 0; i < accLength; i++) {
-            if(accs[i].nature == $scope.accounts[acc]){
+            if (accs[i].nature == $scope.accounts[acc].name) {
                 $scope.comAccounts.push(accs[i]);
             };
         }
