328163eb423a5cfc51b641501d4990e12bd6d5d9
rjabdurrahman <md.rahman0296@gmail.com>
2018-11-09 11:44:36 +0600

    Fixed: Number Validate Backspace key working

--- js/main.js
+++ js/main.js
@@ -49,10 +49,9 @@ function numValidate(evt) {
     } else {
         // Handle key press
         var key = theEvent.keyCode || theEvent.which;
-        key = String.fromCharCode(key);
     }
     var regex = /[0-9]|\./;
-    if (!regex.test(key)) {
+    if (!regex.test(String.fromCharCode(key)) && key != 8) {
         theEvent.returnValue = false;
         if (theEvent.preventDefault) theEvent.preventDefault();
     }
