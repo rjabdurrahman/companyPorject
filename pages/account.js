2ea78c7daf9c7b02ec14a85de3c4395fb32dd783
rjabdurrahman <md.rahman0296@gmail.com>
2018-12-03 21:54:40 +0600

    Added: System to show data from firebase

--- pages/account.js
+++ pages/account.js
@@ -1,15 +1,16 @@
-app.controller('AccountCntlr', function ($scope) {
+app.controller('AccountCntlr', function ($scope, $firebaseArray) {
+    var ref = firebase.database().ref().child('accounts');
     $scope.title = "Account";
     $scope.accounts = accounts;
     lsSetJ('accCodes', $scope.accounts);
-    $scope.comAccounts = [];
-    var accs = lsExGJInit('comAccounts', []);
-    var accLength = lsExGJInit('comAccounts', []).length;
-    for (acc in $scope.accounts) {
-        for (i = 0; i < accLength; i++) {
-            if (accs[i].nature == $scope.accounts[acc].name) {
-                $scope.comAccounts.push(accs[i]);
-            };
-        }
-    }
+    $scope.comAccounts = $firebaseArray(ref);
+    // var accs = lsExGJInit('comAccounts', []);
+    // var accLength = lsExGJInit('comAccounts', []).length;
+    // for (acc in $scope.accounts) {
+    //     for (i = 0; i < accLength; i++) {
+    //         if (accs[i].nature == $scope.accounts[acc].name) {
+    //             $scope.comAccounts.push(accs[i]);
+    //         };
+    //     }
+    // }
 });
