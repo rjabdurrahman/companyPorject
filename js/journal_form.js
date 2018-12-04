366853e84eb9a49b9ad5b01f264399a4bf9e3f96
rjabdurrahman <md.rahman0296@gmail.com>
2018-12-05 01:28:24 +0600

    Added: Journal-form js and remove common journal js

---
+++ js/journal_form.js
@@ -0,0 +1,21 @@
+var accArray = null;
+function ACNameComplete(event) {
+    let data = accArray.find(function (el) {
+        return el.accCode == event.target.value;
+    });
+    // $print(data);
+    if (data) {
+        event.target.parentElement.nextElementSibling.firstElementChild.value = data.accText;
+    }
+    else event.target.parentElement.nextElementSibling.firstElementChild.value = '';
+}
+var partyArray = null;
+function partyNameComplete(event) {
+    for (i = 0; i < partyArray.length; i++) {
+        for (j = 0; j < partyArray[i].length; j++) {
+            if (partyArray[i][j].code == event.target.value) {
+                event.target.parentElement.nextElementSibling.firstElementChild.value = partyArray[i][j].name;
+            }
+        }
+    }
+}
