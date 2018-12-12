37c80944d8560b45ba4cd9af5f960499dba3d5f7
rjabdurrahman <md.rahman0296@gmail.com>
2018-12-13 01:48:55 +0600

    Fixed: debit record total calculator

--- app.js
+++ app.js
@@ -317,7 +317,7 @@ app.controller('LedgerCntlr', function ($scope, $firebaseArray) {
   }
   // Debit Taker End
   $scope.total = function (arr, index) {
-    if (index == 0) return;
+    if (index == -1) return;
     let total = 0;
     for (i = 0; i <= index; i++) {
       total += arr[i].debitCredit[0].drAmount;
