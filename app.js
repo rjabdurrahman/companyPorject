357ca97376bcfcee836eebb97341a3dbf72ad844
rjabdurrahman <md.rahman0296@gmail.com>
2018-12-13 12:48:58 +0600

    Added: Date format invalid alert

--- app.js
+++ app.js
@@ -299,12 +299,8 @@ app.controller('LedgerCntlr', function ($scope, $firebaseArray) {
     let code = e.target.parentElement.previousElementSibling.previousElementSibling.lastElementChild;
     let dateTo = code.parentElement.previousElementSibling.lastElementChild;
     let dateFrom = dateTo.parentElement.previousElementSibling.lastElementChild;
-    if (code.value == "") {
-      $('#notification').html("<h6>Input Account Code</h6>").removeClass('w3-green').addClass('w3-red').fadeIn(200).delay(1000).fadeOut(200);
-      code.focus();
-      return;
-    }
-    else if (dateFrom.value == "") {
+    
+    if (dateFrom.value == "") {
       $('#notification').html("<h6>Input Date From</h6>").removeClass('w3-green').addClass('w3-red').fadeIn(200).delay(1000).fadeOut(200);
       dateFrom.focus();
       return;
@@ -314,6 +310,22 @@ app.controller('LedgerCntlr', function ($scope, $firebaseArray) {
       dateTo.focus();
       return;
     }
+    else if (!(dateTo.value).match(dateEx)) {
+      $('#notification').html("<h6>Date To Format Invalid</h6>").removeClass('w3-green').addClass('w3-red').fadeIn(200).delay(1000).fadeOut(200);
+      dateTo.focus();
+      return;
+    }
+    else if (!(dateFrom.value).match(dateEx)) {
+      $('#notification').html("<h6>Date From Format Invalid</h6>").removeClass('w3-green').addClass('w3-red').fadeIn(200).delay(1000).fadeOut(200);
+      dateFrom.focus();
+      return;
+    }
+    else if (code.value == "") {
+      $('#notification').html("<h6>Input Account Code</h6>").removeClass('w3-green').addClass('w3-red').fadeIn(200).delay(1000).fadeOut(200);
+      code.focus();
+      return;
+    }
+
     $print(dateToNum(dateTo.value));
     $print(dateToNum(dateFrom.value));
     $scope.records = [];
