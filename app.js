db18feab08f3276c2e17c5ad1288db2a5fda9467
rjabdurrahman <md.rahman0296@gmail.com>
2018-12-13 22:59:46 +0600

    Moved: Debit Ledger

--- app.js
+++ app.js
@@ -114,12 +114,12 @@ app.config(function ($routeProvider) {
     })
     .when('/debitledger', {
       templateUrl: 'pages/ledger/debit.html',
-      controller: 'LedgerCntlr',
+      controller: 'DebitLedgerCntlr',
       activetab: 'ledger'
     })
     .when('/creditledger', {
       templateUrl: 'pages/ledger/credit.html',
-      controller: 'LedgerCntlr',
+      controller: 'DebitLedgerCntlr',
       activetab: 'ledger'
     })
     .otherwise({ redirectTo: '/' });
@@ -288,75 +288,4 @@ app.controller('JournalCntlr', function ($scope) {
     if (code == 'Cr') return 'Credit';
   }
   $scope.numToDate = numToDateConv;
-});
-// LedgerCntlr
-app.controller('LedgerCntlr', function ($scope, $firebaseArray) {
-  $scope.title = "Debit Ledger";
-  $scope.numToDate = numToDateConv;
-  $scope.recShow = false;
-  $scope.begBalance = $firebaseArray(getRef('begBalance'));
-  accArrayA = $firebaseArray(getRef('accounts'));
-  $scope.debitTaker = function (e) {
-    let name = e.target.parentElement.previousElementSibling.lastElementChild;
-    let code = name.parentElement.previousElementSibling.lastElementChild;
-    let dateTo = code.parentElement.previousElementSibling.lastElementChild;
-    let dateFrom = dateTo.parentElement.previousElementSibling.lastElementChild;
-    $print(name);
-
-    if (dateFrom.value == "") {
-      $('#notification').html("<h6>Input Date From</h6>").removeClass('w3-green').addClass('w3-red').fadeIn(200).delay(1000).fadeOut(200);
-      dateFrom.focus();
-      return;
-    }
-    else if (!(dateFrom.value).match(dateEx)) {
-      $('#notification').html("<h6>Date From Format Invalid</h6>").removeClass('w3-green').addClass('w3-red').fadeIn(200).delay(1000).fadeOut(200);
-      dateFrom.focus();
-      return;
-    }
-    else if (dateTo.value == "") {
-      $('#notification').html("<h6>Input Date To</h6>").removeClass('w3-green').addClass('w3-red').fadeIn(200).delay(1000).fadeOut(200);
-      dateTo.focus();
-      return;
-    }
-    else if (!(dateTo.value).match(dateEx)) {
-      $('#notification').html("<h6>Date To Format Invalid</h6>").removeClass('w3-green').addClass('w3-red').fadeIn(200).delay(1000).fadeOut(200);
-      dateTo.focus();
-      return;
-    }
-    else if (code.value == "") {
-      $('#notification').html("<h6>Input Account Code</h6>").removeClass('w3-green').addClass('w3-red').fadeIn(200).delay(1000).fadeOut(200);
-      code.focus();
-      return;
-    }
-    else if (name.value == "") {
-      $('#notification').html("<h6>Account Code Invalid</h6>").removeClass('w3-green').addClass('w3-red').fadeIn(200).delay(1000).fadeOut(200);
-      code.focus();
-      return;
-    }
-
-    $print(dateToNum(dateTo.value));
-    $print(dateToNum(dateFrom.value));
-    $scope.records = [];
-    fsDb.collection("JournalForm").where('dACCodes', 'array-contains', code.value).where("date", ">=", dateToNum(dateFrom.value)).where("date", "<=", dateToNum(dateTo.value)).get()
-      .then(function (snapshot) {
-        snapshot.docs.forEach(element => {
-          $scope.records.push(element.data());
-          $scope.$applyAsync();
-          $scope.recShow = true;
-          $print($scope.records);
-        });
-      })
-      .catch(function (err) {
-        $print(err);
-      });
-  }
-  // Debit Taker End
-  $scope.total = function (arr, index) {
-    if (index == -1) return;
-    let total = 0;
-    for (i = 0; i <= index; i++) {
-      total += arr[i].debitCredit[0].drAmount;
-    }
-    return total;
-  }
 });
