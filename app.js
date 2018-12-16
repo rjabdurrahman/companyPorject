739464903cacdbc0f76684c486a3daebaa76c087
rjabdurrahman <md.rahman0296@gmail.com>
2018-12-17 00:59:45 +0600

    Added & Configured: Group by angualar filter

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ['firebase', 'ngRoute']);
+var app = angular.module('myApp', ['firebase', 'ngRoute', 'angular.filter']);
 
 app.config(function ($routeProvider) {
   $routeProvider
