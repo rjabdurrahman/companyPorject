ae989a42b5658df6bea5b83e51da074faf6312f7
rjabdurrahman <md.rahman0296@gmail.com>
2019-01-19 16:32:46 +0600

    Added: GetQueryVAr function

--- js/main.js
+++ js/main.js
@@ -224,6 +224,16 @@ function notify(msg, t) {
     }
 }
 
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) { return pair[1]; }
+    }
+    return (false);
+};
+
 // User Default Permissions
 var dPermission = {
     c_1: false,
