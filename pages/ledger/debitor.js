0c500076ced543b1d92c94bd8bbdaf8b3a6a6408
rjabdurrahman <md.rahman0296@gmail.com>
2019-01-24 23:57:15 +0600

    Added: Party code and remove ac code

--- pages/ledger/debitor.js
+++ pages/ledger/debitor.js
@@ -7,7 +7,6 @@ app.controller('DebitorLedgerCntlr', function ($scope, $firebaseArray) {
     $scope.recShow = false;
     $scope.nodata = false;
     $scope.begBalance = $firebaseArray(getRef('begBalance'));
-    accArrayA = $firebaseArray(getRef('accounts'));
     $scope.debitTaker = function (e) {
         let name = e.target.parentElement.previousElementSibling.lastElementChild;
         let code = name.parentElement.previousElementSibling.lastElementChild;
@@ -99,4 +98,6 @@ app.controller('DebitorLedgerCntlr', function ($scope, $firebaseArray) {
         }
         return total;
     }
+
+    debitorCodes = [];
 });
