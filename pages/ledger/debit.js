29c61b730948528f402551ad3135e6ce3bc7601c
rjabdurrahman <md.rahman0296@gmail.com>
2019-01-30 23:55:55 +0600

    Fixed: Paddy dry print show and debit ledger record show

--- pages/ledger/debit.js
+++ pages/ledger/debit.js
@@ -84,34 +84,34 @@ app.controller('DebitLedgerCntlr', function ($scope, $firebaseArray) {
                 e.target.disabled = false;
                 e.target.textContent = 'Calculate';
             });
-        // fsDb.collection("JournalForm").where('ACCodes', 'array-contains', code.value).where("date", ">=", dateToNum(dateFrom.value)).where("date", "<=", dateToNum(dateTo.value)).get()
-        //     .then(function (snapshot) {
-        //         $scope.recShow = true;
-        //         if (snapshot.size == 0) {
-        //             e.target.disabled = false;
-        //             e.target.textContent = 'Calculate';
-        //             $scope.nodata = true;
-        //             $scope.$applyAsync();
-        //         }
-        //         else {
-        //             snapshot.docs.forEach(element => {
-        //                 let obj = element.data();
-        //                 obj.sCode = code.value;
-        //                 $print(obj);
-        //                 $scope.records.push(obj);
-        //                 $scope.nodata = false;
-        //                 $scope.$applyAsync();
-        //                 $print($scope.records);
-        //                 e.target.disabled = false;
-        //                 e.target.textContent = 'Calculate';
-        //             });
-        //         }
-        //     })
-        //     .catch(function (err) {
-        //         $print(err);
-        //         e.target.disabled = false;
-        //         e.target.textContent = 'Calculate';
-        //     });
+        fsDb.collection("JournalForm").where('ACCodes', 'array-contains', code.value).where("date", ">=", dateToNum(dateFrom.value)).where("date", "<=", dateToNum(dateTo.value)).get()
+            .then(function (snapshot) {
+                $scope.recShow = true;
+                if (snapshot.size == 0) {
+                    e.target.disabled = false;
+                    e.target.textContent = 'Calculate';
+                    $scope.nodata = true;
+                    $scope.$applyAsync();
+                }
+                else {
+                    snapshot.docs.forEach(element => {
+                        let obj = element.data();
+                        obj.sCode = code.value;
+                        $print(obj);
+                        $scope.records.push(obj);
+                        $scope.nodata = false;
+                        $scope.$applyAsync();
+                        $print($scope.records);
+                        e.target.disabled = false;
+                        e.target.textContent = 'Calculate';
+                    });
+                }
+            })
+            .catch(function (err) {
+                $print(err);
+                e.target.disabled = false;
+                e.target.textContent = 'Calculate';
+            });
     }
 
     var mainTotal = 100;
